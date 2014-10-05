fa490ecb34a96c2120f4d4df5071f51dfccfd889
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2014-10-05 22:51:56 +0100

    P 2,3,4

--- main-test.cpp
+++ main-test.cpp
@@ -2,6 +2,25 @@
 #include "lib/gtest-1.7.0/include/gtest/gtest.h"
 
 TEST(P1, LastElementOfList) {
+    EXPECT_EQ(8, lastElementOf({1, 1, 2, 3, 5, 8}));
+}
+
+TEST(P2, PenultimateElementOfList) {
+    EXPECT_EQ(5, penultimate({1, 1, 2, 3, 5, 8}));
+}
+
+TEST(P3, FindNthElementOfList) {
     std::list<int> list = {1, 1, 2, 3, 5, 8};
-    EXPECT_EQ(8, lastElementOf(list));
+    EXPECT_EQ(1, getElement(0, list));
+    EXPECT_EQ(1, getElement(1, list));
+    EXPECT_EQ(2, getElement(2, list));
+    EXPECT_EQ(3, getElement(3, list));
+    EXPECT_EQ(5, getElement(4, list));
+    EXPECT_EQ(8, getElement(5, list));
+}
+
+TEST(P4, NumberOfElementsInList) {
+    EXPECT_EQ(0, sizeOf({}));
+    EXPECT_EQ(1, sizeOf({1}));
+    EXPECT_EQ(6, sizeOf({1, 1, 2, 3, 5, 8}));
 }
--- p99.cpp
+++ p99.cpp
@@ -3,3 +3,24 @@
 int lastElementOf(std::list<int> list) {
     return list.back();
 }
+
+int penultimate(std::list<int> list) {
+    list.pop_back();
+    return lastElementOf(list);
+}
+
+int getElement(int position, std::list<int> list) {
+    std::list<int>::iterator iterator = list.begin();
+    for (int i = 0; i < position; i++) {
+        iterator++;
+    }
+    return *iterator;
+}
+
+int sizeOf(std::list<int> list) {
+    int size = 0;
+    for (std::list<int>::iterator it = list.begin(); it != list.end(); it++) {
+        size++;
+    }
+    return size;
+}
