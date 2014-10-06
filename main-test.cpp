2057a00e8b452430101ae2aa5b5498bdf2503ea3
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2014-10-06 09:07:40 +0100

    P 5,6

--- main-test.cpp
+++ main-test.cpp
@@ -2,11 +2,11 @@
 #include "lib/gtest-1.7.0/include/gtest/gtest.h"
 
 TEST(P1, LastElementOfList) {
-    EXPECT_EQ(8, lastElementOf({1, 1, 2, 3, 5, 8}));
+    EXPECT_EQ(8, lastElementOf((std::list<int>) {1, 1, 2, 3, 5, 8}));
 }
 
 TEST(P2, PenultimateElementOfList) {
-    EXPECT_EQ(5, penultimate({1, 1, 2, 3, 5, 8}));
+    EXPECT_EQ(5, penultimate((std::list<int>) {1, 1, 2, 3, 5, 8}));
 }
 
 TEST(P3, FindNthElementOfList) {
@@ -20,7 +20,28 @@ TEST(P3, FindNthElementOfList) {
 }
 
 TEST(P4, NumberOfElementsInList) {
-    EXPECT_EQ(0, sizeOf({}));
-    EXPECT_EQ(1, sizeOf({1}));
-    EXPECT_EQ(6, sizeOf({1, 1, 2, 3, 5, 8}));
+    EXPECT_EQ(0, sizeOf((std::list<int>) {}));
+    EXPECT_EQ(1, sizeOf((std::list<int>) {1}));
+    EXPECT_EQ(6, sizeOf((std::list<int>) {1, 1, 2, 3, 5, 8}));
+}
+
+TEST(P5, ReverseList) {
+    std::list<int> expected = {};
+    EXPECT_EQ(expected, reverse((std::list<int>) {}));
+
+    expected = {1};
+    EXPECT_EQ(expected, reverse((std::list<int>) {1}));
+
+    expected = {8, 5, 3, 2, 1, 1};
+    EXPECT_EQ(expected, reverse((std::list<int>) {1, 1, 2, 3, 5, 8}));
+}
+
+TEST(P6, IsListAPalindrome) {
+    EXPECT_EQ(true, isPalindrome((std::list<int>) {}));
+    EXPECT_EQ(true, isPalindrome((std::list<int>) {1}));
+    EXPECT_EQ(true, isPalindrome((std::list<int>) {1, 2, 3, 2, 1}));
+
+    std::list<int> list = {1, 1, 2, 3, 5, 8};
+    EXPECT_EQ(false, isPalindrome(list));
+    EXPECT_EQ(6, sizeOf(list));
 }
--- p99.cpp
+++ p99.cpp
@@ -1,26 +1,56 @@
 #include <list>
+#include <iostream>
 
-int lastElementOf(std::list<int> list) {
+template <typename T>
+T lastElementOf(std::list<T> list) {
     return list.back();
 }
 
-int penultimate(std::list<int> list) {
+template <typename T>
+T penultimate(std::list<T> list) {
     list.pop_back();
     return lastElementOf(list);
 }
 
-int getElement(int position, std::list<int> list) {
-    std::list<int>::iterator iterator = list.begin();
+template <typename T>
+T getElement(int position, std::list<T> list) {
+    auto iterator = list.begin();
     for (int i = 0; i < position; i++) {
         iterator++;
     }
     return *iterator;
 }
 
-int sizeOf(std::list<int> list) {
+template <typename T>
+int sizeOf(std::list<T> list) {
     int size = 0;
-    for (std::list<int>::iterator it = list.begin(); it != list.end(); it++) {
+    for (auto it = list.begin(); it != list.end(); it++) {
         size++;
     }
     return size;
+}
+
+template <typename T>
+std::list<T> reverse(std::list<T> list) {
+    std::list<T> result = {};
+    for (auto item : list) {
+        result.push_front(item);
+    }
+    return result;
+}
+
+template <typename T>
+bool isPalindrome(std::list<T> list) {
+    std::list<T> listCopy = list;
+
+    while (listCopy.size() > 1) {
+        T first = listCopy.front();
+        T last = listCopy.back();
+        if (first != last) return false;
+
+        listCopy.pop_front();
+        listCopy.pop_back();
+    }
+    list.clear(); // TODO pass lists by reference
+    return true;
 }
