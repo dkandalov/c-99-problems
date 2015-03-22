9cfc8719fcfa2ef6b38e414ba5775abfd9869c01
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2015-03-22 08:18:09 +0000

    P90

--- p99-misc-gtest.cpp
+++ p99-misc-gtest.cpp
@@ -2,10 +2,19 @@
 #include "p99-misc.cpp"
 
 TEST(P90, EightQueensProblem) {
-    EXPECT_EQ(1, solveEightQueensProblem(1).size());
-    EXPECT_EQ(0, solveEightQueensProblem(2).size());
-    EXPECT_EQ(0, solveEightQueensProblem(3).size());
-    EXPECT_EQ(2, solveEightQueensProblem(4).size());
-    EXPECT_EQ(10, solveEightQueensProblem(5).size());
+    using namespace EightQueens;
+    auto solveAndPrint = [](int boardSize){
+        auto solutions = solveEightQueensProblem(boardSize);
+        for (auto solution : solutions) {
+            std::cout << asString(solution) << "\n";
+        }
+        return solutions;
+    };
+
+    EXPECT_EQ(1, solveAndPrint(1).size());
+    EXPECT_EQ(0, solveAndPrint(2).size());
+    EXPECT_EQ(0, solveAndPrint(3).size());
+    EXPECT_EQ(2, solveAndPrint(4).size());
+    EXPECT_EQ(10, solveAndPrint(5).size());
     EXPECT_EQ(92, solveEightQueensProblem(8).size());
 }
--- p99-misc.cpp
+++ p99-misc.cpp
@@ -6,43 +6,47 @@ template<typename T>
 using Vector = std::vector<T>;
 using String = std::string;
 
-String asString(Vector<int> solution) {
-    String s;
-    for (int col = 0; col < solution.size(); col++) {
-        int row = solution[col];
-        s += "(" + std::to_string(col) + "," + std::to_string(row) + ")";
-    }
-    return s;
-}
-
-bool isValid(Vector<int> solution) {
-    for (int col1 = 0; col1 < solution.size(); col1++) {
-        int row1 = solution[col1];
-        for (int col2 = 0; col2 < solution.size(); col2++) {
-            int row2 = solution[col2];
-            if (col1 == col2) continue;
-            if (row1 == row2) return false;
-            if (abs(row1 - row2) == abs(col1 - col2)) return false;
+namespace EightQueens {
+    String asString(Vector<int> solution) {
+        String s;
+        for (int col = 0; col < solution.size(); col++) {
+            for (int row = 0; row < solution.size(); row++) {
+                if (solution[col] == row) s += "Q"; else s += "-";
+            }
+            s += "\n";
         }
+        return s;
     }
-    return true;
-}
 
-Vector<Vector<int>> solveEightQueensProblem(int boardSize, Vector<int> solution) {
-    if (solution.size() == boardSize) return { solution };
-
-    Vector<Vector<int>> result;
-    for (int row = 0; row < boardSize; row++) {
-        Vector<int> solutionCopy(solution);
-        solutionCopy.push_back(row);
-        if (isValid(solutionCopy)) {
-            auto subResult = solveEightQueensProblem(boardSize, solutionCopy);
-            result.insert(result.end(), subResult.begin(), subResult.end());
+    bool isValid(Vector<int> solution) {
+        for (int col1 = 0; col1 < solution.size(); col1++) {
+            int row1 = solution[col1];
+            for (int col2 = 0; col2 < solution.size(); col2++) {
+                int row2 = solution[col2];
+                if (col1 == col2) continue;
+                if (row1 == row2) return false;
+                if (abs(row1 - row2) == abs(col1 - col2)) return false;
+            }
         }
+        return true;
     }
-    return result;
-}
 
-Vector<Vector<int>> solveEightQueensProblem(int boardSize) {
-    return solveEightQueensProblem(boardSize, {});
+    Vector<Vector<int>> solveEightQueensProblem(int boardSize, Vector<int> solution) {
+        if (solution.size() == boardSize) return { solution };
+
+        Vector<Vector<int>> result;
+        for (int row = 0; row < boardSize; row++) {
+            Vector<int> solutionCopy(solution);
+            solutionCopy.push_back(row);
+            if (isValid(solutionCopy)) {
+                auto subResult = solveEightQueensProblem(boardSize, solutionCopy);
+                result.insert(result.end(), subResult.begin(), subResult.end());
+            }
+        }
+        return result;
+    }
+
+    Vector<Vector<int>> solveEightQueensProblem(int boardSize) {
+        return solveEightQueensProblem(boardSize, {});
+    }
 }
